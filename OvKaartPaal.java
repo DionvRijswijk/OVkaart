a3f59b9833082294181ab887ee39ac3c1fe0c234
DionvRijswijk <1212869@student.roc-nijmegen.nl>
2026-04-14 12:11:44 +0200

    end of day commit

--- OvKaartPaal.java
+++ OvKaartPaal.java
@@ -25,26 +25,28 @@ public class OvKaartPaal {
 //		kaart.uitchecken(afstand);
 		
 		if (kaart.getGeldigheid() == true) {
-
+			System.out.print("1");
 			if (kaart.getIngecheckt() == true) {
-
+				System.out.print("2");
 //				if (kaart.getSaldo() > kosten) {
 
 					System.out.print("Uitchecken voltooid");
-					
+					System.out.print("3");
 					kaart.uitchecken(afstand);
 //				} else {
 //					System.out.print("Niet genoeg saldo");
 				}
 			} else {
 				System.out.print("Kaart is nog niet ingecheckt");
+				System.out.print("4");
 			}
 		}  {
 			System.out.print("Kaart is niet geldig");
+			System.out.print("5");
 		}
 		
 	
-	
+		
 	
 	
 	// functie die een kaart incheckt
@@ -52,24 +54,30 @@ public class OvKaartPaal {
 	public void inchecken(OvKaart kaart) {
 
 		if (kaart.getGeldigheid() == true) {
+			System.out.print("6");
 
 			if (kaart.getIngecheckt() == false) {
-
+				System.out.print("7");
 				if (kaart.getSaldo() > kosten) {
-
+					System.out.print("8");
 					System.out.print("welkom aanboord");
 					
 					kaart.inchecken(kosten, locatie);
 				} else {
+					System.out.print("9");
 					System.out.print("Niet genoeg saldo");
 				}
 			} else {
+				System.out.print("10");
 				System.out.print("Kaart is al ingecheckt");
 			}
 		} else {
+			System.out.print("11");
 			System.out.print("Kaart is niet geldig");
 		}
 
 	}
+	
+	
 
 }
